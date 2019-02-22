c995235f36886186ae0d562f1be88dd175ec21a9
skywalker <huangpengssdut@126.com>
2019-02-22 22:53:50 +0800

    top K

---
+++ interviews/src/main/java/com/dlut/interviews/top/TopK.java
@@ -0,0 +1,153 @@
+/*
+ * Copyright (c) 2010-2015 meituan.com
+ * All rights reserved.
+ * 
+ */
+package com.dlut.interviews.top;
+
+import java.util.Arrays;
+
+/**
+ * @author ray
+ *         date 2019年02月22日
+ */
+public class TopK {
+
+
+    public static int[] topK(int[] arr, int topK) {
+        if (arr == null || arr.length <= topK) {
+            return arr;
+        }
+
+        if (topK == 0) {
+            return null;
+        }
+
+        int[] runArr = Arrays.copyOf(arr, arr.length);
+
+        return topK(runArr, 0, runArr.length - 1, topK);
+    }
+
+    private static int[] topK(int arr[], int beginIndex, int endIndex, int topK) {
+        if (topK == 1) {
+            return topOne(arr, beginIndex, endIndex);
+
+        } else if (topK == 2) {
+            return topTwo(arr, beginIndex, endIndex);
+
+        } else {
+
+            int pIndex = partition(arr, beginIndex, endIndex);
+
+            if (endIndex - pIndex == topK) {
+                // [pIndex + 1, endIndex]
+                return Arrays.copyOfRange(arr, pIndex + 1, endIndex + 1);
+
+            } else if (endIndex - pIndex + 1 == topK) {
+                // [pIndex, endIndex]
+                return Arrays.copyOfRange(arr, pIndex, endIndex + 1);
+
+            } else if (endIndex - pIndex > topK) {
+                // topK(arr, pIndex + 1, endIndex, topK)
+                return topK(arr, pIndex + 1, endIndex, topK);
+
+            } else if (endIndex - pIndex + 1 < topK) {
+                // [pIndex, endIndex] + topK(arr, beginIndex, pIndex - 1, topK - (endIndex - pIndex + 1))
+                int[] leftTop = topK(arr, beginIndex, pIndex - 1, topK - (endIndex - pIndex + 1));
+                int[] rightTop = Arrays.copyOfRange(arr, pIndex, endIndex + 1);
+                return mergeArr(leftTop, rightTop);
+
+            } else {
+                throw new IllegalStateException("unexpected condition");
+            }
+        }
+    }
+
+    private static int[] mergeArr(int[] arr1, int[] arr2) {
+        int[] resultArr = new int[arr1.length + arr2.length];
+
+        int index = 0;
+        for (int element : arr1) {
+            resultArr[index++] = element;
+        }
+
+        for (int element : arr2) {
+            resultArr[index++] = element;
+        }
+
+        return resultArr;
+    }
+
+    /**
+     * find top 2 number at range [beginIndex, endIndex]
+     *
+     * @param arr
+     * @param beginIndex
+     * @param endIndex
+     * @return
+     */
+    private static int[] topTwo(int[] arr, int beginIndex, int endIndex) {
+        int[] topTwoArr = new int[]{Integer.MIN_VALUE, Integer.MIN_VALUE};
+
+        for (int i = beginIndex; i <= endIndex; i++) {
+            if (arr[i] > topTwoArr[1]) {
+                topTwoArr[1] = arr[i];
+
+                // sure topTwoArr[0] > topTwoArr[1]
+                if (topTwoArr[1] > topTwoArr[0]) {
+                    swap(topTwoArr, 0, 1);
+                }
+            }
+        }
+
+        return topTwoArr;
+    }
+
+    /**
+     * find top 1 number at range [beginIndex, endIndex]
+     *
+     * @param arr
+     * @param beginIndex
+     * @param endIndex
+     * @return
+     */
+    private static int[] topOne(int[] arr, int beginIndex, int endIndex) {
+        int max = Integer.MIN_VALUE;
+        for (int i = beginIndex; i <= endIndex; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+
+        return new int[]{max};
+    }
+
+
+    private static int partition(int[] arr, int beginIndex, int endIndex) {
+        int key = arr[beginIndex];
+
+        while (beginIndex < endIndex) {
+
+            // 可以更改 arr[endIndex] <= key, 改成 partition 右边的数比 arr[partition] 小, 实现找反向的 topK
+            while (beginIndex < endIndex && arr[endIndex] >= key) {
+                endIndex--;
+            }
+            swap(arr, beginIndex, endIndex);
+
+
+            // 可以更改 arr[beginIndex] >= key, 改成 partition 右边的数比 arr[partition] 小, 实现找反向的 topK
+            while (beginIndex < endIndex && arr[beginIndex] <= key) {
+                beginIndex++;
+            }
+            swap(arr, beginIndex, endIndex);
+        }
+
+        return beginIndex;
+    }
+
+    private static void swap(int[] arr, int i, int k) {
+        int temp = arr[i];
+        arr[i] = arr[k];
+        arr[k] = temp;
+    }
+}
